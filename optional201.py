964f56f510df45e0a7726cb29f7385f61ebd871c
khilling <55695412+khilling@users.noreply.github.com>
2020-01-07 23:54:07 +0300

    Add files via upload

--- optional201.py
+++ optional201.py
@@ -1,4 +1,4 @@
-def letter(letter, level, way):                    #проверяет есть ли такое начало уже
+def letter(letter, level, way):                    #РїСЂРѕРІРµСЂСЏРµС‚ РµСЃС‚СЊ Р»Рё С‚Р°РєРѕРµ РЅР°С‡Р°Р»Рѕ СѓР¶Рµ
     global parent_index
     for j in range(len(tree[level][way])):
         if tree[level][way][j][0] == letter:
@@ -6,7 +6,7 @@ def letter(letter, level, way):                    #проверяет есть ли такое нача
     return parent_way, parent_index, None, way
 
 
-def word(array):                                         #выводит ответ
+def word(array):                                         #РІС‹РІРѕРґРёС‚ РѕС‚РІРµС‚
     answering = []
     for answer in range(array[0][0], -1, -1):
         answering.append(tree[answer][array[0][1]][array[0][2]][0])
@@ -15,8 +15,8 @@ def word(array):                                         #выводит ответ
 
 
 f = open('new.txt', encoding = 'utf-8')
-file = f.read().split()                       #tree[порядок буквы - 1][нужная ветка с нужным началом][буква][данные]
-tree = [[[['', 0, 0, 0, 0]]], [[['а', 0, 0, 0, -1]]]] #[буква, сколько раз слово, индекс путя родителя, индекс родителя, индекс массива с детьми]
+file = f.read().split()                       #tree[РїРѕСЂСЏРґРѕРє Р±СѓРєРІС‹ - 1][РЅСѓР¶РЅР°СЏ РІРµС‚РєР° СЃ РЅСѓР¶РЅС‹Рј РЅР°С‡Р°Р»РѕРј][Р±СѓРєРІР°][РґР°РЅРЅС‹Рµ]
+tree = [[[['', 0, 0, 0, 0]]], [[['Р°', 0, 0, 0, -1]]]] #[Р±СѓРєРІР°, СЃРєРѕР»СЊРєРѕ СЂР°Р· СЃР»РѕРІРѕ, РёРЅРґРµРєСЃ РїСѓС‚СЏ СЂРѕРґРёС‚РµР»СЏ, РёРЅРґРµРєСЃ СЂРѕРґРёС‚РµР»СЏ, РёРЅРґРµРєСЃ РјР°СЃСЃРёРІР° СЃ РґРµС‚СЊРјРё]
 
 for item in file:
     n = len(item)
@@ -24,25 +24,25 @@ for item in file:
     parent_index = 0
     parent_way = 0
     way = 0
-    while way != None and way > -1 and i < n:           #иду пока не наткнусь на отсутствие буквы или на конец пути или на конец слова
+    while way != None and way > -1 and i < n:           #РёРґСѓ РїРѕРєР° РЅРµ РЅР°С‚РєРЅСѓСЃСЊ РЅР° РѕС‚СЃСѓС‚СЃС‚РІРёРµ Р±СѓРєРІС‹ РёР»Рё РЅР° РєРѕРЅРµС† РїСѓС‚Рё РёР»Рё РЅР° РєРѕРЅРµС† СЃР»РѕРІР°
         parent_way, parent_index, way, last_way = letter(item[i], i + 1, way)
         i += 1
-    if way == None:                 #если изначально было отсутствие буквы
-        tree[i][last_way].append([item[i - 1], 0, parent_way, parent_index, -1])         #добавляю букву
+    if way == None:                 #РµСЃР»Рё РёР·РЅР°С‡Р°Р»СЊРЅРѕ Р±С‹Р»Рѕ РѕС‚СЃСѓС‚СЃС‚РІРёРµ Р±СѓРєРІС‹
+        tree[i][last_way].append([item[i - 1], 0, parent_way, parent_index, -1])         #РґРѕР±Р°РІР»СЏСЋ Р±СѓРєРІСѓ
         parent_way, parent_index = last_way, len(tree[i][last_way]) - 1
-    for k in range(i, n):                  #добавляю отстаток слова
-        if k + 1 >= len(tree):                               #если такого длинного слова еще не было
+    for k in range(i, n):                  #РґРѕР±Р°РІР»СЏСЋ РѕС‚СЃС‚Р°С‚РѕРє СЃР»РѕРІР°
+        if k + 1 >= len(tree):                               #РµСЃР»Рё С‚Р°РєРѕРіРѕ РґР»РёРЅРЅРѕРіРѕ СЃР»РѕРІР° РµС‰Рµ РЅРµ Р±С‹Р»Рѕ
             tree.append([[[item[k], 0, parent_way, parent_index, -1]]])
             tree[k][parent_way][parent_index][4] = len(tree[k + 1]) - 1
             parent_way, parent_index = 0, 0
-        else:                                                #если уже было такое длинное
+        else:                                                #РµСЃР»Рё СѓР¶Рµ Р±С‹Р»Рѕ С‚Р°РєРѕРµ РґР»РёРЅРЅРѕРµ
             tree[k + 1].append([[item[k], 0, parent_way, parent_index, -1]])
             tree[k][parent_way][parent_index][4] = len(tree[k + 1]) - 1
             parent_way, parent_index = len(tree[k + 1]) - 1, 0
-    if i == n and way != None:                               #если такое слово уже было
+    if i == n and way != None:                               #РµСЃР»Рё С‚Р°РєРѕРµ СЃР»РѕРІРѕ СѓР¶Рµ Р±С‹Р»Рѕ
         tree[n][parent_way][parent_index][1] += 1
     else:
-        tree[n][-1][-1][1] += 1                #прибавляю слово к последней букве
+        tree[n][-1][-1][1] += 1                #РїСЂРёР±Р°РІР»СЏСЋ СЃР»РѕРІРѕ Рє РїРѕСЃР»РµРґРЅРµР№ Р±СѓРєРІРµ
     
 maximum = [[0, 0, 0], 0]
 for level in range(1, len(tree)):
